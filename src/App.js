dd47aea655c24dde7924caffde1f19845324b819
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-17 17:50:44 -0400

    Error Elements

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } 
 // pages
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Faq from "./pages/help/Faq";
+import Contact from "./pages/help/Contact";
+import NotFound from "./pages/NotFound";
+import Careers, { careersLoader } from "./pages/careers/Careers";
+import CareerDetails, { careerDetailsLoader } from "./pages/careers/CareerDetails";
+import CareersError from "./pages/careers/CareersError";
 
 // layouts
 import RootLayout from "./layouts/RootLayout";
 import HelpLayout from "./layouts/HelpLayout";
-import Faq from "./pages/help/Faq";
-import Contact from "./pages/help/Contact";
-import NotFound from "./pages/NotFound";
 import CareersLayout from "./layouts/CareerLayout";
-import Careers, { careersLoader } from "./pages/careers/Careers";
-import CareerDetails, { careerDetailsLoader } from "./pages/careers/CareerDetails";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,7 +27,7 @@ const router = createBrowserRouter(
         <Route path="contact" element={<Contact />} />
       </Route>
 
-      <Route path="careers" element={<CareersLayout />}>
+      <Route path="careers" element={<CareersLayout />} errorElement={<CareersError />} >
         <Route index element={<Careers />} loader={careersLoader} />
         <Route path=":id" element={<CareerDetails />} loader={careerDetailsLoader} />
       </Route>
